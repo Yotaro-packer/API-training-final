c6ce4a8878f7ec9a43a43a8013bf8a0230258bc6
Yotaro- <20124024@kaishi-pu.ac.jp>
2026-01-29 01:37:41 +0900

    ランキング機能のみのリリース

--- ranklogger-api/models/types.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type GameRecordInput struct {
-	UUID      string                 `json:"uuid" validate:"required,uuid4"`       // UUID形式か
-	PlayCount int                    `json:"play_count" validate:"required,min=1"` // 1以上か
-	Data      map[string]interface{} `json:"data" validate:"required"`
-}
